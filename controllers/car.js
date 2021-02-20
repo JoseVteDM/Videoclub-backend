622a87e429d1a9cc42838705986b34d51109495f
Jose Vte. Díaz Mora <74594028+JVDMKKHHPMY@users.noreply.github.com>
2021-02-20 11:30:43 +0100

    car controller

---
+++ controllers/car.js
@@ -0,0 +1,34 @@
+
+const Storage = require('../storage');
+
+class Car {
+
+    storage;
+
+    constructor() {
+        this.storage = new Storage();
+    }
+
+    async indexAll() {
+        return this.storage.get();
+    }
+
+    async store(car) {
+        return this.storage.set(car);
+    }
+
+    async update(id, car) {    
+        return this.storage.updateById(id,car);
+    }
+
+    async destroy(id) {
+        return this.storage.deleteById(id);
+    }
+
+
+
+}
+
+
+let carController = new Car();
+module.exports = carController;
