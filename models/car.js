9dc0c3b63d63e1d6e706ac7c8d2dafbb0cdbe4c3
Jose Vte. Díaz Mora <74594028+JVDMKKHHPMY@users.noreply.github.com>
2021-02-20 11:52:04 +0100

    añadimos storage

---
+++ models/car.js
@@ -0,0 +1,15 @@
+class Car {
+
+    brand;
+    model;
+    year;
+
+    constructor(car){
+        this.brand = car.brand;
+        this.model = car.model;
+        this.year = car.year;
+    }
+
+}
+
+module.exports = Car;
